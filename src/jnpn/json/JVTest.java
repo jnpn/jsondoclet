cf8dd85639636382c3a17361b16d6c8cb2b45a03
Johan PONIN <johan.ponin.pro@gmail.com>
2019-10-02 20:51:29 +0200

    Added Gson uses in JVTest to ensure proper classpath

--- src/jnpn/json/JVTest.java
+++ src/jnpn/json/JVTest.java
@@ -1,8 +1,23 @@
 package jnpn.json;
 
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * JVTest: jar main class
+ *  - prints a prologue and a json array [1,2,3]
+ *  - used to test classpath issues mostly
+ *
+ * @class JVTest: jar main class to test classpath issues
+ *
+ **/
 public class JVTest {
     public static void main(String[] args) {
 	// var v = new JVisitor();
 	System.out.println("jsondoclet-bis (c) jnpn 2019-");
+	var g = new GsonBuilder().setPrettyPrinting().create();
+	var j = g.toJson(List.of(1,2,3));
+	System.out.println(j);
     }
 }
