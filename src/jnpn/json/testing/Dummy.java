c1324c98fa2c25c0272e596a8f87bfa3f0ac7cc0
Johan PONIN <johan.ponin.pro@gmail.com>
2019-10-04 12:33:40 +0200

    trying jackson in jnpn.json.testing.Dummy

--- src/jnpn/json/testing/Dummy.java
+++ src/jnpn/json/testing/Dummy.java
@@ -1,8 +1,14 @@
 package jnpn.json.testing;
 
 import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import jnpn.json.visitors.Bean;
+import com.fasterxml.jackson.core.JsonProcessingException;
+////// com/fasterxml/jackson/databind/ObjectMapper.class
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Dummy: jar main class
@@ -16,8 +22,25 @@ public class Dummy {
     public static void main(String[] args) {
 	// var v = new JVisitor();
 	System.out.println("jsondoclet-bis (c) jnpn 2019-");
+
+	System.out.println("gson");
 	var g = new GsonBuilder().setPrettyPrinting().create();
 	var j = g.toJson(List.of(1,2,3));
 	System.out.println(j);
+
+	System.out.println("jackson");
+
+	try {
+	    var b = new Bean();
+	    System.out.println("pojo -> " + b);
+	    var s = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(b);
+	    System.out.println("json -> " + s);
+	}
+	catch (JsonProcessingException e) {
+	    System.out.println("Error " + e.getMessage());
+	    e.printStackTrace();
+	}
+
+	System.out.println("bye");
     }
 }
