87a61c1f08b22ee4e6f028c344cbf974b4805ac8
Johan PONIN <johan.ponin.pro@gmail.com>
2019-10-04 06:19:37 +0200

    changing packages.py innards: no glob, use pathlib
    - glob wasn't general enough, I made up bad tricks to walk subdirs
    - using os.walk instead, very simple and solid
    - using pathlib and os aware global variables for path syntax

--- scripts/packages.py
+++ scripts/packages.py
@@ -1,38 +1,63 @@
 #!/usr/bin/env python
 
-# import os
-
-# def packages(root,psep=' ', ext='*.java'):
-#     sep = os.path.sep
-#     return psep.join(os.path.sep.join([top, d, ext]) for top,ds,fs in os.walk(root) for d in ds)
-
 import os
 import sys
 import glob
+import pathlib
+import functools
 
 DEBUG = False
+
+################################################################### PRELUDE
 
 def dbg(*a, **k):
     if DEBUG:
         print('[debug]', end=' ')
         print(*a)
 
-def sources(root, sep=' ', pattern='**/*.java'):
-    matches = glob.glob(root + pattern)
-    return sep.join(matches)
-
-ROOT='src/jnpn/json/'
-def main():
-    print(sources(ROOT), sources(ROOT, pattern='*.java'))
-
 def zipmap(a,b,f):
     return [f(i,j) for i,j in zip(a,b)]
 
-def match(a, b, strict=False):
-    return all(zipmap(a,b,lambda u,v: u == v))
-
 def vmatch(a, b, var=None):
     return all(zipmap(a,b,lambda u,v: u is var or v is var or u == v))
+
+################################################################### CORE
+
+# def sources(root, sep=' ', pattern=os.path.sep.join(['**','*.java'])):
+#     ''' Path > str > str -> str'''
+#     assert isinstance(root, pathlib.Path)
+#     r = str(root / pattern)
+#     dbg(r)
+#     matches = glob.glob(r)
+#     return sep.join(matches)
+
+def s(root, pred):
+    return ((top,fn)
+            for top,dirs,files in os.walk(root)
+            for fn in files
+            if pred(top, fn))
+
+def sourceset(cli, ext='.java'):
+    ''' flag -> path -> str<os> '''
+    flag, root, *_ = cli
+    root = pathlib.Path(root)
+    dbg('sourceset', flag, root, _)
+    fn_format = lambda t,f: t + os.path.sep + f
+    set_format = lambda a,b: a + ' ' + b
+    files = s(root, lambda t,f: f.endswith(ext))
+    return functools.reduce(set_format, (fn_format(t,f) for t,f in files))
+    
+def libs(cli, ext='.jar'):
+    ''' flag -> path -> str<os> '''
+    flag, root, *_ = cli
+    root = pathlib.Path(root)
+    sep = os.path.pathsep
+    dbg('libs', flag, root, _)
+    dbg(sep)
+    fn_format = lambda t,f: t + os.path.sep + f
+    set_format = lambda a,b: a + os.path.pathsep + b
+    files = s(root, lambda t,f: f.endswith(ext))
+    return functools.reduce(set_format, ((t + os.path.sep + f) for t,f in files))
 
 def usage():
     print(__file__, ':')
@@ -40,20 +65,20 @@ def usage():
     print(__file__, '--source', '<dir>')
     print(__file__, '--libs', '<dir>')
 
-if __name__ == '__main__':
+ROOT=pathlib.Path('src') / 'jnpn' / 'json'
+def main():
     dbg(os.getcwd())
+    dbg(sys.platform)
     this, *cli = sys.argv
     if cli:
         if vmatch(cli, ['--sources', None]):
-            main()
+            print(sourceset(cli))
         elif vmatch(cli, ['--libs', None]):
-            flag, root, *_ = cli
-            dbg(flag, root, _)
-            sep = ':'
-            toplevel = sources(root, sep=sep, pattern='**/*.jar')
-            deeplevel = sources(root, sep=sep, pattern='**/**/*.jar')
-            print(sep.join([toplevel, deeplevel]))
+            print(libs(cli))
         else:
             print('wat')
     else:
         usage()
+
+if __name__ == '__main__':
+    main()
